dbcd7f04e143a30bd524ba442d73f5311aa1bbd2
thlim008 <37361242+thlim008@users.noreply.github.com>
2026-01-27 09:19:04 +0900

    Delete app directory

--- app/missing_pets/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MissingPetsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app.missing_pets'
-    label = 'missing_pets'
